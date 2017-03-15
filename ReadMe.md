538f1e10de21102b6ac0d03be50f3e84216b0bd7
HoossenMH <ghasan16@gmail.com>
2017-03-15 17:37:08 +0200

    Initial commit

---
+++ ReadMe.md
@@ -0,0 +1,3 @@
+Muhammad Hasan Hoossen
+213223538
+First Maven Project
---
+++ src/main/java/hoossen/App.java
@@ -0,0 +1,13 @@
+package hoossen;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
